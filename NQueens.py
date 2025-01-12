57226cf706d2bfac84071eae3dbdcb054f7275d8
FranK1308 <117226763+FranK1308@users.noreply.github.com>
2025-01-12 13:26:16 +0100

    Update NQueens.py

--- NQueens.py
+++ NQueens.py
@@ -4,6 +4,9 @@ The n-queens puzzle is the problem of placing n queens on an n x n chessboard su
 Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any order.
 
 Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space, respectively.
+
+Constraints:
+1 <= n <= 9
 """
 
 def solveNQueens(n: int):
